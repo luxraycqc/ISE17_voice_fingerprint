49672f49cc6384daea56013b027b8239dbb0a723
luxraycqc <1120132106@bit.edu.cn>
2018-01-03 09:55:22 +0800

    Update server.py

--- Project_code/server/server.py
+++ Project_code/server/server.py
@@ -29,7 +29,7 @@ def recognize_file():
         print 'found a file', f
         print 'recognized File', recognizedFile
         #djv.fingerprint_file(f)
-    return str(recognizedFile) or 'No file found'
+    	return str(recognizedFile) or 'No file found'
 
 @app.route('/fingerprint', methods=['GET', 'POST'])
 def fingerprint_file():
@@ -38,7 +38,7 @@ def fingerprint_file():
         f.save('/home/andrewcao/PycharmProjects/server/tmpfiles/' + secure_filename(f.filename))
         recognizedFile = djv.fingerprint_file('/home/andrewcao/PycharmProjects/server/tmpfiles/'+ secure_filename(f.filename))
         print 'recognized File', recognizedFile
-    return str(recognizedFile) or 'No file found'
+    	return str(recognizedFile) or 'No file found'
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -56,7 +56,7 @@ def register_user():
         file3.save('/home/andrewcao/PycharmProjects/server/tmpfiles/' + username + '_'+ secure_filename(file3.filename))
         recognizedFile = djv.fingerprint_file('/home/andrewcao/PycharmProjects/server/tmpfiles/'+ username + '_'+ secure_filename(file3.filename))
 	
-    return str(recognizedFile) or 'User not registered'
+    	return str(recognizedFile) or 'User not registered'
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate_user():
